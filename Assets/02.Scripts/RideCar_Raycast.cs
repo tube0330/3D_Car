9b999315efe3fc692d895202b59c0c5e5197f05f
tube0330 <zien0516@naver.com>
2024-08-05 19:35:14 +0900

    240805_Raycast

--- Assets/02.Scripts/RideCar_Raycast.cs
+++ Assets/02.Scripts/RideCar_Raycast.cs
@@ -4,30 +4,22 @@ using UnityEngine;
 
 public class RideCar_Raycast : MonoBehaviour
 {
-    public float raycastDistance = 5f; // 레이캐스트 거리
-    public LayerMask carLayer; // car 레이어
+    public int carLayer = LayerMask.NameToLayer("Car");
+    Camera cam;
+    RideCar C_ridecar;
+
+    void Start()
+    {
+        cam = this.gameObject.GetComponentInChildren<Camera>();
+        C_ridecar = GetComponent<RideCar>();
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, 25f, carLayer))
         {
-            RaycastHit hit;
-            // 카메라 앞쪽에서 레이캐스트 발사
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out hit, raycastDistance, carLayer))
-            {
-                // car 레이어의 오브젝트와 충돌했을 때
-                GetInCar(hit.transform);
-            }
+            if (Input.GetKeyDown(KeyCode.E))
+                C_ridecar.PlayerGetInCar();
         }
-    }
-
-    void GetInCar(Transform carTransform)
-    {
-        // 차량 탑승 로직 구현
-        // 예시: 플레이어를 차량의 자식 오브젝트로 만들고 위치를 조정
-        transform.SetParent(carTransform);
-        transform.localPosition = Vector3.zero;
     }
 }
